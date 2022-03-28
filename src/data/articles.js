94b67ced05efc22fdeb4340ad1aaa8fed11f7302
Tony <tonymike99@gmail.com>
2022-03-28 19:26:21 +0530

    data: created data for articles, categories and products

---
+++ src/data/articles.js
@@ -0,0 +1,24 @@
+import { v4 as uuid } from "uuid";
+
+const articleData = [
+  {
+    _id: uuid(),
+    src: "https://unsplash.com/photos/GREEBEtyR9Y/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjJ8fHQlMjBzaGlydHxlbnwwfDF8fHwxNjQ0NjQyMDI1&force=true&w=640",
+    alt: "A man in a white t-shirt",
+    title: "Tony",
+    profession: "Author",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, corrupti!",
+  },
+  {
+    _id: uuid(),
+    src: "https://unsplash.com/photos/GREEBEtyR9Y/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjJ8fHQlMjBzaGlydHxlbnwwfDF8fHwxNjQ0NjQyMDI1&force=true&w=640",
+    alt: "A man in a white t-shirt",
+    title: "Tony",
+    profession: "Author",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, corrupti!",
+  },
+];
+
+export { articleData };
