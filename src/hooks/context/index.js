17b2c2d96c66de202f352e5bdb9e57e8cc69c362
Tony <tonymike99@gmail.com>
2022-03-31 23:25:38 +0530

    updated indexes

--- src/hooks/context/index.js
+++ src/hooks/context/index.js
@@ -1,1 +1,3 @@
 export { FilterProvider, useFilter } from "./filter-context";
+export { ThemeProvider, useTheme } from "./theme-context";
+export { CartProvider, useCart } from "./cart-context";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 export { Login } from "./Login/Login";
 export { Signup } from "./Signup/Signup";
 export { Products } from "./Products/Products";
+export { Cart } from "./Cart/Cart";
 export { MockAPI } from "./MockAPI/MockAPI";
 export { PageNotFound } from "./PageNotFound/PageNotFound";
