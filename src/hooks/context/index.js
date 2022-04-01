ac198344fe0bfb4d952806196c977ebe40bdca25
Tony <tonymike99@gmail.com>
2022-04-01 21:22:54 +0530

    updated index files

--- src/hooks/context/index.js
+++ src/hooks/context/index.js
@@ -1,3 +1,4 @@
+export { WishlistProvider, useWishlist } from "./wishlist-context";
+export { CartProvider, useCart } from "./cart-context";
 export { FilterProvider, useFilter } from "./filter-context";
 export { ThemeProvider, useTheme } from "./theme-context";
-export { CartProvider, useCart } from "./cart-context";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 export { Login } from "./Login/Login";
 export { Signup } from "./Signup/Signup";
+export { Home } from "./Home/Home";
 export { Products } from "./Products/Products";
+export { Wishlist } from "./Wishlist/Wishlist";
 export { Cart } from "./Cart/Cart";
 export { MockAPI } from "./MockAPI/MockAPI";
 export { PageNotFound } from "./PageNotFound/PageNotFound";
