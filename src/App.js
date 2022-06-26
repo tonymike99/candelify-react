96b1c0d6471b77ae372dbc73354d57eb87b764d8
Tony <tonymike99@gmail.com>
2022-06-26 21:26:10 +0530

    added private and restricted routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { PrivateRoute, RestrictedRoute } from "./auth/index";
 import { Footer, Header } from "./components/index";
 import {
   Login,
@@ -19,12 +20,19 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/mock-api" element={<MockAPI />} />
+
+        <Route element={<RestrictedRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/mock-api" element={<MockAPI />} />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/products" element={<Products />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/cart" element={<Cart />} />
+        </Route>
+
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
 
